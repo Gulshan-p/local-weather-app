fa74d0d0b6329932ceaf9e3e8b7a33682e1c97a3
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-02-21 00:25:59 -0800

    calling open weather map API

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-weather',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
 
-  constructor() { }
+  constructor() {
+    this.current = {
+      city: 'Redmond',
+      country: 'usa',
+      date: new Date(),
+      image: '',
+      temprature: 30,
+      description: 'snowy',
+    }
+   }
 
   ngOnInit(): void {
   }
